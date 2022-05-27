a12bbf30dad8a2892aa9bf649e74bd740fc20885
elevenfofiv <elevenfofiv@outlook.com>
2022-05-27 16:19:55 +0900

    event

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const age = parseInt(prompt("How Old Are You?"));
-
-if (isNaN(age) || age < 0) {
-  console.log("please input the positive number");
-} else if (age < 20) {
-  console.log("You are too young");
-} else if (age < 60) {
-  console.log("You are my friends");
-} else if (age < 80) {
-  console.log("you are a little bit old");
-} else if (age > 80) {
-  console.log("You are so old");
+const title = document.querySelector(".hello h1");
+function handleTilteClick() {
+  title.style.color = "blue";
 }
+
+function handleTitleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone";
+}
+
+title.addEventListener("click", handleTilteClick);
+title.addEventListener("mouseenter", handleTitleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
