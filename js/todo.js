a8d4fd81f567694406067b3af5dd15fe979ab8ca
elevenfofiv <elevenfofiv@outlook.com>
2022-06-03 17:13:13 +0900

    JS 완료

--- js/todo.js
+++ js/todo.js
@@ -12,7 +12,7 @@ function saveToDos() {
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
-  console.log(li.id);
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   li.remove();
 }
 
